4adc670a5f1fea2b91e917ce1b06e3aa197329da
jgottschalkmhs <49209767+jgottschalkmhs@users.noreply.github.com>
2020-07-20 07:40:50 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta name="description" content="Chic Clothing">
+	<meta name="keywords" content="Chic Clothing">
+	<meta name="author" content="Miss Gottschalk (from Phil Adams' videos) and more than a little help from Dave">
+	<meta charset="UTF-8">
+	<title>Chic Clothing</title>
+	<link rel="stylesheet" href="theme/chic.css" title="style1" />
+</head>
+	
+	<body>
+<div class="content">
+
+	<div class="heading">
+		<h1 class="white">Chic Clothing</h1>
+	</div> <!-- end heading -->
+
+	<div class="navigation">
+		
+		Category links go here | 
+					
+		<a class="nav" href="admin/admin.php">Admin</a>
+		
+	</div>	<!-- end navigation -->
+
+	<div class="main">
+
+		<h1>Welcome to Chic Clothing</h1>
+
+		<p>Please choose a category from the list above or <a href="#">login</a> to access the admin panel.</p>	
+
+	</div> <!-- end main -->
+		
+	<div class="footer">
+		&copy; Miss Gottschalk 2015
+	</div> <!-- end footer -->
+
+</div> <!-- end content -->
+
+</body>
+
+
+</html>
